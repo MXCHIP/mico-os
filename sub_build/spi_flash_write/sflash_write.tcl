f3050b67bbe77c82307851535be5508a3ccddff6
swyang <yangsw@mxchip.com>
2017-04-17 16:57:22 +0800

    修复下载寻找flash_prog.elf路径的问题。

--- sub_build/spi_flash_write/sflash_write.tcl
+++ sub_build/spi_flash_write/sflash_write.tcl
@@ -125,8 +125,17 @@ proc sflash_init { PlatBusDebug } {
     reset halt
     
     #load_image build/sub_build.spi_flash_write@NoRTOS@$PlatBusDebug/binary/sub_build.spi_flash_write@NoRTOS@$PlatBusDebug.elf
-    load_image mico-os/board/$PlatBusDebug/flash_prog.elf
-
+    if {[file exists mico-os/board/$PlatBusDebug/flash_prog.elf]} {
+        load_image mico-os/board/$PlatBusDebug/flash_prog.elf
+    } else {
+        if {[file exists board/$PlatBusDebug/flash_prog.elf]} {
+            load_image board/$PlatBusDebug/flash_prog.elf
+        } else {
+            error "Error: Cann't find flash_prog.elf in $PlatBusDebug directory"
+            exit -1;            
+        }
+    }
+    
     #mdw 0x000D0000 100
     #mdw $entry_address_loc 100
 
