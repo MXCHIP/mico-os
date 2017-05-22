21793eb2022d2cfd3353c148f2f9e629b6b69423
顾杨杰 <guyj@mxchip.com>
2017-05-22 16:17:34 +0800

    add borad AZ3166; add TARGET_STM32F412xG

--- platform/mbed/targets/TARGET_STM/TARGET_STM32F4/TARGET_STM32F4.mk
+++ platform/mbed/targets/TARGET_STM/TARGET_STM32F4/TARGET_STM32F4.mk
@@ -17,7 +17,8 @@ HOST_OPENOCD := stm32f4x
 
 # Add MiCO extended device drivers
 $(NAME)_SOURCES := peripherals/platform_flash_embed.c \
-                   peripherals/platform_flash.c
+                   peripherals/platform_flash.c \
+                   peripherals/platform_flash_qspi.c
 
 ifndef NO_WIFI
 $(NAME)_SOURCES += wlan_bus_driver/wlan_bus_$(BUS).c \
