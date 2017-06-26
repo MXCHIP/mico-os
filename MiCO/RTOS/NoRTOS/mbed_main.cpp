732de7dbd4eb259cee2d25eba7d8e04e51bc29b0
lizhibo32 <17602115406@163.com>
2017-06-26 11:22:57 +0800

    恢复mbed_main.cpp

--- MiCO/RTOS/NoRTOS/mbed_main.cpp
+++ MiCO/RTOS/NoRTOS/mbed_main.cpp
@@ -61,7 +61,8 @@ static volatile uint32_t no_os_tick = 0;
 Timer noos_timer;
 
 void noos_tick_handler() {
-    platform_watchdog_kick( );
+    no_os_tick ++;
+    //platform_watchdog_kick( );
 }
 
 extern "C" void mbed_main( void )
