f505c018344420174f289616cfe0376e9d344afd
William Xu <xuwei@mxchip.com>
2017-07-11 21:15:45 +0800

    Enable Tickless mode in FreeRTOS 9.0

--- MiCO/RTOS/FreeRTOS/ver9.0.0/FreeRTOSConfig.h
+++ MiCO/RTOS/FreeRTOS/ver9.0.0/FreeRTOSConfig.h
@@ -108,6 +108,10 @@
 #define configIDLE_SHOULD_YIELD		1
 #define configUSE_CO_ROUTINES 		0
 #define configMAX_CO_ROUTINE_PRIORITIES ( 2 )
+#define configUSE_TICKLESS_IDLE       (1)
+
+extern void rtos_suppress_and_sleep( unsigned long sleep_ms );
+#define portSUPPRESS_TICKS_AND_SLEEP( xExpectedIdleTime )    rtos_suppress_and_sleep( xExpectedIdleTime )
 
 /* Hooks */
 #define configUSE_IDLE_HOOK			0
