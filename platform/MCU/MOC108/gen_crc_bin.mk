ac4b12bf4fe66038df0d72db25c5f3d2fa77f0cb
杨诗伟 <yangsw@mxchip.com>
2017-09-25 09:55:26 +0800

    编译时生成 3060 的 all.bin

--- platform/MCU/MOC108/gen_crc_bin.mk
+++ platform/MCU/MOC108/gen_crc_bin.mk
@@ -1,3 +1,4 @@
+EXTRA_POST_BUILD_TARGETS += gen_standard_images
 
 ifeq ($(HOST_OS),Win32)
 ENCRYPT := "$(SOURCE_ROOT)/mico-os/platform/MCU/MOC108/encrypt_win.exe"
@@ -21,16 +22,38 @@ endif # Linux64
 endif # Linux32
 endif # Win32
 
+#bootloader
+BOOT_BIN_FILE := $(MICO_OS_PATH)/resources/moc_kernel/$(MODULE)/boot.bin
+BOOT_OFFSET   := 0x0
+
+#application 
+APP_BIN_FILE := $(BIN_OUTPUT_FILE)
+APP_OFFSET := 0x13200
+
+#ate firmware
+ATE_BIN_FILE := $(MICO_OS_PATH)/resources/ate_firmware/$(MODULE)/ate.bin
+ATE_OFFSET := 0xA1090
+
+# _crc.bin
 CRC_BIN_OUTPUT_FILE :=$(LINK_OUTPUT_FILE:$(LINK_OUTPUT_SUFFIX)=_crc$(BIN_OUTPUT_SUFFIX))
+# _crc.bin.xz
 CRC_XZ_BIN_OUTPUT_FILE :=$(LINK_OUTPUT_FILE:$(LINK_OUTPUT_SUFFIX)=_crc$(BIN_OUTPUT_SUFFIX).xz)
+# .ota.bin
 OTA_BIN_OUTPUT_FILE := $(LINK_OUTPUT_FILE:$(LINK_OUTPUT_SUFFIX)=.ota$(BIN_OUTPUT_SUFFIX))
 
-EXTRA_POST_BUILD_TARGETS += gen_crc_bin
+# Required to build Full binary file
+GEN_COMMON_BIN_OUTPUT_FILE_SCRIPT:= $(SCRIPTS_PATH)/gen_common_bin_output_file.py
 
-gen_crc_bin:
+MICO_ALL_BIN_OUTPUT_FILE :=$(LINK_OUTPUT_FILE:$(LINK_OUTPUT_SUFFIX)=.all$(BIN_OUTPUT_SUFFIX))
+
+gen_standard_images: 
 	$(eval OUT_MSG := $(shell $(ENCRYPT) $(BIN_OUTPUT_FILE) 0 0 0 0; \
 	$(XZ) --lzma2=dict=32KiB --check=crc32 -k $(CRC_BIN_OUTPUT_FILE); \
 	$(CP) $(CRC_XZ_BIN_OUTPUT_FILE) $(OTA_BIN_OUTPUT_FILE); \
+	$(CP) $(CRC_BIN_OUTPUT_FILE) $(BIN_OUTPUT_FILE); \
 	$(RM) $(CRC_BIN_OUTPUT_FILE); \
-	$(RM) $(CRC_XZ_BIN_OUTPUT_FILE); \
-	$(RM) $(BIN_OUTPUT_FILE)))
+	$(RM) $(CRC_XZ_BIN_OUTPUT_FILE);))
+	$(QUIET)$(RM) $(MICO_ALL_BIN_OUTPUT_FILE)
+	$(PYTHON) $(GEN_COMMON_BIN_OUTPUT_FILE_SCRIPT) -o $(MICO_ALL_BIN_OUTPUT_FILE) -f $(BOOT_OFFSET) $(BOOT_BIN_FILE)              
+	$(PYTHON) $(GEN_COMMON_BIN_OUTPUT_FILE_SCRIPT) -o $(MICO_ALL_BIN_OUTPUT_FILE) -f $(APP_OFFSET)  $(APP_BIN_FILE)
+	$(PYTHON) $(GEN_COMMON_BIN_OUTPUT_FILE_SCRIPT) -o $(MICO_ALL_BIN_OUTPUT_FILE) -f $(ATE_OFFSET)  $(ATE_BIN_FILE)
