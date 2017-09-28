988798ac21ed7367a799fc654e9f2233481e7566
杨诗伟 <yangsw@mxchip.com>
2017-09-28 11:33:39 +0800

    修复了 3060 重复 make 会导致 .bin 重复 CRC 的问题。

--- platform/MCU/MOC108/gen_crc_bin.mk
+++ platform/MCU/MOC108/gen_crc_bin.mk
@@ -1,4 +1,4 @@
-EXTRA_POST_BUILD_TARGETS += gen_standard_images
+EXTRA_POST_BUILD_TARGETS += $(MICO_ALL_BIN_OUTPUT_FILE)
 
 ifeq ($(HOST_OS),Win32)
 CRC := "$(SOURCE_ROOT)/mico-os/platform/MCU/MOC108/tools/crc/win/crc.exe"
@@ -22,6 +22,13 @@ endif # Linux64
 endif # Linux32
 endif # Win32
 
+# _crc.bin
+CRC_BIN_OUTPUT_FILE :=$(LINK_OUTPUT_FILE:$(LINK_OUTPUT_SUFFIX)=_crc$(BIN_OUTPUT_SUFFIX))
+# _crc.bin.xz
+CRC_XZ_BIN_OUTPUT_FILE :=$(LINK_OUTPUT_FILE:$(LINK_OUTPUT_SUFFIX)=_crc$(BIN_OUTPUT_SUFFIX).xz)
+# .ota.bin
+OTA_BIN_OUTPUT_FILE := $(LINK_OUTPUT_FILE:$(LINK_OUTPUT_SUFFIX)=.ota$(BIN_OUTPUT_SUFFIX))
+
 #bootloader
 BOOT_BIN_FILE := $(MICO_OS_PATH)/resources/moc_kernel/$(MODULE)/boot.bin
 BOOT_OFFSET   := 0x0
@@ -34,29 +41,23 @@ APP_OFFSET := 0x13200
 ATE_BIN_FILE := $(MICO_OS_PATH)/resources/ate_firmware/$(MODULE)/ate.bin
 ATE_OFFSET := 0xA1090
 
-# _crc.bin
-CRC_BIN_OUTPUT_FILE :=$(LINK_OUTPUT_FILE:$(LINK_OUTPUT_SUFFIX)=_crc$(BIN_OUTPUT_SUFFIX))
-# _crc.bin.xz
-CRC_XZ_BIN_OUTPUT_FILE :=$(LINK_OUTPUT_FILE:$(LINK_OUTPUT_SUFFIX)=_crc$(BIN_OUTPUT_SUFFIX).xz)
-# .ota.bin
-OTA_BIN_OUTPUT_FILE := $(LINK_OUTPUT_FILE:$(LINK_OUTPUT_SUFFIX)=.ota$(BIN_OUTPUT_SUFFIX))
-
 # Required to build Full binary file
 GEN_COMMON_BIN_OUTPUT_FILE_SCRIPT:= $(SCRIPTS_PATH)/gen_common_bin_output_file.py
 
 MICO_ALL_BIN_OUTPUT_FILE :=$(LINK_OUTPUT_FILE:$(LINK_OUTPUT_SUFFIX)=.all$(BIN_OUTPUT_SUFFIX))
 
-gen_crc_bin: build_done
-	$(eval OUT_MSG := $(shell $(CRC) $(BIN_OUTPUT_FILE) 0 0 0 0))
+$(CRC_BIN_OUTPUT_FILE): $(STRIPPED_LINK_OUTPUT_FILE)
+	$(QUIET)$(OBJCOPY) -O binary -R .eh_frame -R .init -R .fini -R .comment -R .ARM.attributes $(STRIPPED_LINK_OUTPUT_FILE) $(BIN_OUTPUT_FILE)
+	$(CRC) $(BIN_OUTPUT_FILE) 0 0 0 0 > $(DEV_NULL)
 
-gen_xz_bin: gen_crc_bin
-	$(eval OUT_MSG := $(shell $(XZ) --lzma2=dict=32KiB --check=crc32 -k $(CRC_BIN_OUTPUT_FILE)))
-
-gen_standard_images: gen_crc_bin gen_xz_bin
-	$(CP) $(CRC_BIN_OUTPUT_FILE) $(BIN_OUTPUT_FILE)
-	$(RM) $(CRC_BIN_OUTPUT_FILE)
+$(OTA_BIN_OUTPUT_FILE): $(CRC_BIN_OUTPUT_FILE)
+	$(XZ) --lzma2=dict=32KiB --check=crc32 -k $(CRC_BIN_OUTPUT_FILE)
 	$(CP) $(CRC_XZ_BIN_OUTPUT_FILE) $(OTA_BIN_OUTPUT_FILE)
 	$(RM) $(CRC_XZ_BIN_OUTPUT_FILE)
+
+$(MICO_ALL_BIN_OUTPUT_FILE): $(OTA_BIN_OUTPUT_FILE)
+	$(CP) $(CRC_BIN_OUTPUT_FILE) $(BIN_OUTPUT_FILE)
+	$(RM) $(CRC_BIN_OUTPUT_FILE)
 	$(QUIET)$(RM) $(MICO_ALL_BIN_OUTPUT_FILE)
 	$(PYTHON) $(GEN_COMMON_BIN_OUTPUT_FILE_SCRIPT) -o $(MICO_ALL_BIN_OUTPUT_FILE) -f $(BOOT_OFFSET) $(BOOT_BIN_FILE)              
 	$(PYTHON) $(GEN_COMMON_BIN_OUTPUT_FILE_SCRIPT) -o $(MICO_ALL_BIN_OUTPUT_FILE) -f $(APP_OFFSET)  $(APP_BIN_FILE)
