ed3ec9f373ac189b822abf57859a9702751d4bd0
杨海波 <yanghb@mxchip.com>
2017-12-26 19:49:55 +0800

    1、更新MOC108的库文件，合并beken 12月25日的patch；softap最大支持8个client； 2、修改button的处理逻辑，把falling irq和rising irq分开，同时MOC108的irq需要对第一次做特殊处理。解决了moc108的GPIO IRQ工作不稳定的问题。

--- libraries/drivers/keypad/gpio_button/button.c
+++ libraries/drivers/keypad/gpio_button/button.c
@@ -32,50 +32,60 @@ typedef struct _button_context_t{
   button_pressed_cb pressed_func;
   button_long_pressed_cb long_pressed_func;
   uint32_t start_time;
+#ifdef CONFIG_MX108  
+  int      ignore;
+#endif
 } button_context_t;
 
 static button_context_t context[5];
 
-//typedef void (*_button_irq_handler)( void* arg );
+static void button_rising_irq_handler( void* arg );
+static void button_rising_irq_handler( void* arg );
 
-static void button_irq_handler( void* arg )
+static void button_falling_irq_handler( void* arg )
 {
-  button_context_t *_context = arg;
-
-  int interval = -1;
-  
-  if ( MicoGpioInputGet( _context->gpio ) == 0 ) {
-    MicoGpioEnableIRQ( _context->gpio, IRQ_TRIGGER_RISING_EDGE, button_irq_handler, _context );
+    button_context_t *_context = arg;
+#ifdef CONFIG_MX108
+    if (_context->ignore == 1) {
+      _context->ignore = 0;
+      return;
+    }
+#endif
+    MicoGpioEnableIRQ( _context->gpio, IRQ_TRIGGER_RISING_EDGE, button_rising_irq_handler, _context );
     _context->start_time = mico_rtos_get_time()+1;
     mico_rtos_start_timer(&_context->_user_button_timer);
-  } else {
+}
+static void button_rising_irq_handler( void* arg )
+{
+    button_context_t *_context = arg;
+    int interval = -1;
+    
+#ifdef CONFIG_MX108  
+    if (_context->ignore == 1) {
+        _context->ignore = 0;
+        return;
+    }
+#endif    
     interval = (int)mico_rtos_get_time() + 1 - _context->start_time ;
     if ( (_context->start_time  != 0) && interval > 50 && interval < _context->timeout){
       /* button clicked once */
       if( _context->pressed_func != NULL )
         (_context->pressed_func)();
     }
-    MicoGpioEnableIRQ( _context->gpio, IRQ_TRIGGER_FALLING_EDGE, button_irq_handler, _context );
+    MicoGpioEnableIRQ( _context->gpio, IRQ_TRIGGER_FALLING_EDGE, button_falling_irq_handler, _context );
     mico_rtos_stop_timer(&_context->_user_button_timer);
     _context->start_time  = 0;
-  }
 }
-
-void (*button_irq_handler_array[5])() = {button_irq_handler, button_irq_handler, button_irq_handler, button_irq_handler, button_irq_handler};
-
 
 static void button_timeout_handler( void* arg )
 {
   button_context_t *_context = arg;
-
+  
   _context->start_time = 0;
   if( _context->long_pressed_func != NULL )
     (_context->long_pressed_func)();
+
 }
-
-void (*button_timeout_handler_array[5])() = {button_timeout_handler, button_timeout_handler, button_timeout_handler, button_timeout_handler, button_timeout_handler};
-
-
 
 void button_init( int index, button_init_t init)
 {
@@ -85,14 +95,21 @@ void button_init( int index, button_init_t init)
   context[index].pressed_func = init.pressed_func;
   context[index].long_pressed_func = init.long_pressed_func;
 
-  #ifdef NUCLEO_F412ZG_EASYLINK_BUTTON
-	  MicoGpioInitialize( init.gpio, INPUT_PULL_DOWN );
-	  MicoGpioEnableIRQ( init.gpio, IRQ_TRIGGER_RISING_EDGE, button_irq_handler_array[index], &context[index] );
-  #else
-	  MicoGpioInitialize( init.gpio, INPUT_PULL_UP );
-	  MicoGpioEnableIRQ( init.gpio, IRQ_TRIGGER_FALLING_EDGE, button_irq_handler_array[index], &context[index] );
-	#endif
-  mico_rtos_init_timer( &context[index]._user_button_timer, init.long_pressed_timeout, button_timeout_handler_array[index], &context[index] );
+#ifdef NUCLEO_F412ZG_EASYLINK_BUTTON
+	MicoGpioInitialize( init.gpio, INPUT_PULL_DOWN );
+	MicoGpioEnableIRQ( init.gpio, IRQ_TRIGGER_RISING_EDGE, button_rising_irq_handler, &context[index] );
+#else
+	MicoGpioInitialize( init.gpio, INPUT_PULL_UP );
+#ifdef CONFIG_MX108  
+    /*For MOC108 only. MOC108 will trigger an IRQ if IO is in low state when enable falling IRQ*/
+    if (MicoGpioInputGet(init.gpio) == 0)
+    context[index].ignore = 1;
+    else
+    context[index].ignore = 0;
+#endif   
+	MicoGpioEnableIRQ( init.gpio, IRQ_TRIGGER_FALLING_EDGE, button_falling_irq_handler, &context[index] );
+  #endif
+  mico_rtos_init_timer( &context[index]._user_button_timer, init.long_pressed_timeout, button_timeout_handler, &context[index] );
 }
 
 
