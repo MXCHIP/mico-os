f55b31f04086ab63b1c1857b1b4a21ae6dfca368
杨海波 <yanghb@mxchip.com>
2018-03-26 15:37:37 +0800

    更新3080B的kernel，增加2个timer的API： int mico_change_timer_period( mico_timer_t* timer, uint32_t new_period ) int mico_init_once_timer( mico_timer_t* timer, uint32_t time_ms, timer_handler_t function, void* arg )

--- platform/MCU/MX1290/moc/moc_adapter.c
+++ platform/MCU/MX1290/moc/moc_adapter.c
@@ -463,3 +463,13 @@ int hardfault_get(char *msg, int len)
     return _kernel_api.os_apis->hardfault_get(msg, len);
 }
 
+int mico_change_timer_period( mico_timer_t* timer, uint32_t new_period )
+{
+    return _kernel_api.os_apis->mico_change_timer_period(timer, new_period );
+}
+
+int mico_init_once_timer( mico_timer_t* timer, uint32_t time_ms, timer_handler_t function, void* arg )
+{
+    return _kernel_api.os_apis->mico_init_once_timer( timer, time_ms, function, arg );
+}
+
