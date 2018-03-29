3ec207701a667dde8f6c3dead5039a880cc60abb
丁全全 <dingqq@mxchip.com>
2018-03-29 10:08:59 +0800

    提高timer的优先级

--- MiCO/RTOS/FreeRTOS/ver9.0.0/FreeRTOSConfig.h
+++ MiCO/RTOS/FreeRTOS/ver9.0.0/FreeRTOSConfig.h
@@ -96,7 +96,7 @@
 
 /* Timers */
 #define configUSE_TIMERS                            ( 1 )
-#define configTIMER_TASK_PRIORITY                   ( 2 )
+#define configTIMER_TASK_PRIORITY                   ( 4 )
 #define configTIMER_QUEUE_LENGTH                    ( 5 )
 #ifndef configTIMER_TASK_STACK_DEPTH
 #define configTIMER_TASK_STACK_DEPTH                ( ( unsigned short ) (1024 / sizeof( portSTACK_TYPE )) )
