9000f78853e0fbdb105a1186535bb0bc4c3bca2d
yangsw <yangsw@mxchip.com>
2018-10-11 14:04:29 +0800

    更新 fortuen

--- makefiles/scripts/fortune.py
+++ makefiles/scripts/fortune.py
@@ -6,11 +6,33 @@
 import sys
 import zlib
 import random
+import argparse
 
-with open('mico-os/makefiles/scripts/fortune.dat', 'rb') as f:
-    c = f.read()
+def main(argv):
+    parser = argparse.ArgumentParser(description='Show/Compress/Uncompress fortune')
+    parser.add_argument('-x', '--command' , type=str, required=True, help='Board')
+    parser.add_argument('-i', '--input'	, type=str, required=True, help='File name')
+    parser.add_argument('-o', '--output'  , type=str, required=False, help='Address')
+    args = parser.parse_args(argv)
 
-lines = zlib.decompress(c).splitlines()
-line = lines[random.randint(0 ,len(lines)-1)]
-line = line.decode('UTF-8').encode('GBK') if sys.platform == 'win32' else line
-print('\r\n'+line+'\r\n')
+    with open(args.input, 'rb') as f:
+        c = f.read()
+
+    if args.command == 'show':
+        lines = zlib.decompress(c).splitlines()
+        line = lines[random.randint(0 ,len(lines)-1)]
+        line = line.decode('UTF-8').encode('GBK') if sys.platform == 'win32' else line
+        print('\r\n'+line+'\r\n')
+    elif args.command == 'compress':
+        with open(args.output, 'wb') as f:
+            f.write(zlib.compress(c))
+    elif args.command == 'decompress':
+        with open(args.output, 'wb') as f:
+            f.write(zlib.decompress(c))
+
+if __name__ == '__main__':
+	try:
+		main(sys.argv[1:])
+	except Exception as e:
+		print(e)
+		sys.exit(2)
